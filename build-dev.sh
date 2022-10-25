8a0ba0177a19d9cff3eb7df846becbd446391b81
dcole <dcole@redhat.com>
2022-10-25 12:39:10 -0600

    cleanup step 5

---
+++ build-dev.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# This file could be run from a CICD pipeline to assert
+# the developer/builder environment is setup correctly
+# and ready to build container images.
+
+# Install Podman and/or Docker
+# create args for choosing either one
+
+# 1. add the podman/docker repo's to the local machine
+
+# 2. download/install podman/docker
+
+# 3. Install/assert python >=3.8
+
+# 4. install/assert PIP --version is for python >=3.8
+
+# 5. Install virtualenv
+
+# 6. verify 
